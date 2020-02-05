d7646c57cbefb24e0432a8b4bfb6b3511e8c5356
Chris Kerbo <chris.kerbo@outlook.com>
2020-02-05 20:42:34 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,8 @@ class ProductsController < ApplicationController
   def index
   end
 
-  def add_to_cart
-    # Get the item from the path
-    @item = Item.find(params[:id])
-
-    # Load the cart from the session, or create a new empty cart.
-    current_cart << @item.id
+  def add
+    cart << params[:product]
   end
 
 end
